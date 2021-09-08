bc78b827e8550cc1f396da3fb464a630716ab04f
RocinRykor <briggs.steven369@gmail.com>
2021-09-08 00:49:14 -0700

    misc

--- src/main/java/studio/rrprojects/decryptbot/audio/TTSHandler.java
+++ src/main/java/studio/rrprojects/decryptbot/audio/TTSHandler.java
@@ -29,14 +29,16 @@ public class TTSHandler {
         // Build the voice request, select the language code ("en-US") and the ssml voice gender
         // ("neutral")
         voice = VoiceSelectionParams.newBuilder()
-                .setName("en-GB-Wavenet-B") //Specify a voice beyond just language code - Allows selection of Wavenet (Premium) voices over standard ones (I think Wavenet voices are selected automatically)
-                .setLanguageCode("en-GB")
+                .setName("en-US-Wavenet-I") //Specify a voice beyond just language code - Allows selection of Wavenet (Premium) voices over standard ones (I think Wavenet voices are selected automatically)
+                .setLanguageCode("en-US")
                 .setSsmlGender(SsmlVoiceGender.MALE)
                 .build();
 
         // Select the type of audio file you want returned
         audioConfig = AudioConfig.newBuilder()
                 .setAudioEncoding(AudioEncoding.MP3)
+                .setPitch(-9)
+                //.setSpeakingRate(0.8)
                 .build();
 
 		/*
